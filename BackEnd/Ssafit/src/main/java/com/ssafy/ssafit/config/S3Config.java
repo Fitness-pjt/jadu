3a36dc45d5bbfa210ffab533512623a9517fffc3
MINSSUNGKIM <planman0@gmail.com>
2024-11-17 12:56:16 +0900

    [Refactor] 암호화

--- BackEnd/Ssafit/src/main/java/com/ssafy/ssafit/config/S3Config.java
+++ BackEnd/Ssafit/src/main/java/com/ssafy/ssafit/config/S3Config.java
@@ -1,5 +1,6 @@
 package com.ssafy.ssafit.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,17 +11,18 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-    
-    @Bean
-    public S3Client s3Client() {
-        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(
-            "AKIAX5ZI6BHGHBQABL5I", 
-            "I/8JHfIg02DDWBZ4N2nPRAmWrsKj5RKjgcFtcBV3"
-        );
-        
-        return S3Client.builder()
-            .region(Region.AP_NORTHEAST_2) // 서울 리전
-            .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
-            .build();
-    }
+
+	@Value("${cloud.aws.credentials.access-key}")
+	private String accessKey;
+
+	@Value("${cloud.aws.credentials.secret-key}")
+	private String secretKey;
+
+	@Bean
+	public S3Client s3Client() {
+		AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+		return S3Client.builder().region(Region.AP_NORTHEAST_2) // 서울 리전
+				.credentialsProvider(StaticCredentialsProvider.create(awsCredentials)).build();
+	}
 }
