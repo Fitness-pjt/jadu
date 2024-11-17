e2f70855ad01b198a356da5d0f34d219ec71ff8b
MINSSUNGKIM <planman0@gmail.com>
2024-11-17 12:55:47 +0900

    [Docs] DB수정

--- MySQL/DB_DDL.sql
+++ MySQL/DB_DDL.sql
@@ -107,10 +107,9 @@ CREATE TABLE `answer` (
 -- File management
 CREATE TABLE `files` (
     `file_id`  INT NOT NULL AUTO_INCREMENT,
-    `file_case` ENUM('PROFILE', 'BOARD', 'QUESTION', 'REVIEW'),
+    `file_case` ENUM('PROFILE', 'BOARD', 'QUESTION'),
     `file_name` VARCHAR(100) NOT NULL,
     `extension` VARCHAR(20) NOT NULL,
-    `file_path` VARCHAR(255) NOT NULL,
     `user_id` INT NOT NULL,
     `question_id` INT,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -123,8 +122,8 @@ CREATE TABLE `files` (
 CREATE TABLE `todo` (
     `todo_id` INT NOT NULL AUTO_INCREMENT,
     `user_id` INT NOT NULL,
-    `program_id` INT NOT NULL,
-    `video_id` VARCHAR(50) NOT NULL,  
+    `program_id` INT,
+    `video_id` VARCHAR(50),  
     `date` DATE NOT NULL,
     `status` BOOLEAN DEFAULT FALSE,
     `content` VARCHAR(100) NOT NULL,
